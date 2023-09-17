1442faa3cb61c93b1054fed61e5f10eb5cc73edb
John Dye <geekydevguy@gmail.com>
2023-09-17 16:36:38 -0400

    it works, kind of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,225 @@
-import Image from 'next/image'
+"use client"
+
+import axios, {Axios} from "axios";
+import {useEffect} from "react";
+
 
 export default function Home() {
+
+    let inputNodes : Node[] = [];
+
+    function getNamedInputs(baseElement: Node) : Node[] {
+        let elements = baseElement.childNodes;
+        let foundElements : Node[] = [];
+        elements.forEach( x =>
+        {
+            let result = getNamedInputs(x);
+            foundElements =  foundElements.concat(result);
+        });
+        if(baseElement.nodeName == "INPUT")
+        {
+            foundElements.push(baseElement);
+        }
+        return foundElements;
+    }
+
+    function initialize() {
+        console.log("initializing");
+        let elements = document.childNodes;
+        let foundElements : Node[] = []
+        for (let element of elements)
+        {
+            let result = getNamedInputs(element);
+            foundElements = foundElements.concat(result);
+        }
+
+        inputNodes = foundElements;
+    }
+
+    useEffect(() => {
+        initialize();
+    });
+
+    class Inputs {
+       square: string;
+       value: string;
+       constructor(square: string, value: string)
+       {
+           this.square = square;
+           this.value = value;
+       }
+    }
+
+    function resetBoard() {
+        for(let node of inputNodes)
+        {
+            let inputTag = node as HTMLInputElement;
+            inputTag.value = "";
+            inputTag.classList.remove("answered")
+        }
+    }
+
+    function getSolution() {
+
+        let submission : Inputs[] = [];
+
+        // get the values from the input nodes
+        for(let node of inputNodes)
+        {
+            let inputTag = node as HTMLInputElement;
+            submission.push(new Inputs(inputTag.name, inputTag.value))
+        }
+
+        console.log("Submitting")
+        axios.post("https://localhost:7236/Solver", submission)
+             .then(x => processSolverResults(x.data))
+    }
+
+    function processSolverResults (returnResponse: any) {
+        console.log("processing results")
+        for(let node of inputNodes)
+        {
+            let inputTag = node as HTMLInputElement;
+            // get the matching name from the return response.
+           let match = returnResponse.updatedValues.find(x  => x.square == inputTag.name)
+            if(match != undefined)
+            {
+                inputTag.classList.add("answered");
+                inputTag.value = match.value;
+            }
+        }
+    }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <div className="flex-row">
+          <input className="numberinput" type="text" maxLength={1} name="x-1-y-1"></input>
+          <input className="numberinput" type="text" maxLength={1} name="x-2-y-1"></input>
+          <input className="numberinput" type="text" maxLength={1} name="x-3-y-1"></input>
+          <div className="vdivider"/>
+          <input className="numberinput" type="text" maxLength={1} name="x-4-y-1"></input>
+          <input className="numberinput" type="text" maxLength={1} name="x-5-y-1"></input>
+          <input className="numberinput" type="text" maxLength={1} name="x-6-y-1"></input>
+          <div className="vdivider"/>
+          <input className="numberinput" type="text" maxLength={1} name="x-7-y-1"></input>
+          <input className="numberinput" type="text" maxLength={1} name="x-8-y-1"></input>
+          <input className="numberinput" type="text" maxLength={1} name="x-9-y-1"></input>
+      </div>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-2"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-2"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-2"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-2"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-2"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-2"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-2"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-2"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-2"></input>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-3"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-3"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-3"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-3"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-3"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-3"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-3"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-3"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-3"></input>
+        </div>
+        <div className="hdivider"/>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-4"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-4"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-4"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-4"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-4"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-4"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-4"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-4"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-4"></input>
+        </div>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-5"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-5"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-5"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-5"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-5"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-5"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-5"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-5"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-5"></input>
+        </div>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-6"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-6"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-6"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-6"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-6"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-6"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-6"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-6"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-6"></input>
+        </div>
+        <div className="hdivider"/>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-7"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-7"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-7"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-7"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-7"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-7"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-7"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-7"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-7"></input>
+        </div>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-8"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-8"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-8"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-8"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-8"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-8"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-8"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-8"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-8"></input>
+        </div>
+        <div className="inputrow">
+            <input className="numberinput" type="text" maxLength={1} name="x-1-y-9"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-2-y-9"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-3-y-9"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-4-y-9"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-5-y-9"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-6-y-9"></input>
+            <div className="vdivider"/>
+            <input className="numberinput" type="text" maxLength={1} name="x-7-y-9"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-8-y-9"></input>
+            <input className="numberinput" type="text" maxLength={1} name="x-9-y-9"></input>
+        </div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div><button className="solver" name="solveButton" onClick={(_ => getSolution())} >Solve</button>
+            <span>&nbsp;&nbsp;</span>
+        <button className="solver" name="resetButton" onClick={(_ => resetBoard())}>Reset</button>
+        </div>
     </main>
   )
+
 }
